288204ea1cb40a0057155e551a9a2cef456ed1f6
Nyxiaz <dishasingh3108@gmail.com>
2026-06-12 10:23:05 +0530

    added background and player

--- main.py
+++ main.py
@@ -2,18 +2,45 @@ import pygame
 import time
 import random
 
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 960, 540
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Get pew-pew'd")
 
+BG = pygame.transform.scale(
+    pygame.image.load("Background.png"), (WIDTH, HEIGHT)
+)
+
+PLAYER_WIDTH = 200
+PLAYER_HEIGHT = 200
+
+PLAYER = pygame.image.load("gura.png")
+PLAYER = pygame.transform.scale(PLAYER, (PLAYER_WIDTH, PLAYER_HEIGHT))
+
+
+def draw():
+    WIN.blit(BG, (0, 0))
+
+    WIN.blit(
+        PLAYER,
+        (WIDTH // 2 - PLAYER_WIDTH // 2,
+         HEIGHT - PLAYER_HEIGHT + 30)
+    )
+
+    pygame.display.update()
+
+
 def main():
     run = True
+
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-                break
+
+        draw()
+
     pygame.quit()
+
 
 if __name__ == "__main__":
     main()
